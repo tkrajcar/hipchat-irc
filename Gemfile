521fdc3563de6d54a838ba3bc0254b9e837acb18
Tim Krajcar <allegro@conmolto.org>
2014-12-18 11:16:11 -0800

    add dotenv support

--- Gemfile
+++ Gemfile
@@ -4,3 +4,4 @@ ruby '2.1.5'
 
 gem 'cinch', '~> 2.1.0'
 gem 'hipchat', '~> 1.4.0'
+gem 'dotenv'
--- main.rb
+++ main.rb
@@ -1,6 +1,8 @@
 require 'rubygems'
 require 'bundler/setup'
 require 'cinch'
+require 'dotenv'
+Dotenv.load
 
 require './hipchat_messenger'
 
